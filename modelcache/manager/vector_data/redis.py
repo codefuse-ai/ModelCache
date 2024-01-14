32091a625a0e374c7e88000d2d09bd6bd6880bd1
fuhui <hongen.phe@antgroup.com>
2024-01-14 10:53:24 +0800

    add redis init py

---
+++ modelcache/manager/vector_data/redis.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+   Alipay.com Inc.
+   Copyright (c) 2004-2023 All Rights Reserved.
+   ------------------------------------------------------
+   File Name : redis.py
+   Author : fuhui.phe
+   Create Time : 2024/1/14 10:53
+   Description : description what the main function of this file
+   Change Activity: 
+        version0 : 2024/1/14 10:53 by fuhui.phe  init
+"""
