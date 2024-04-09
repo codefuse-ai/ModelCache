4636fb2e3f17a60d99081e8408a8dfe593c0a9d5
fuhui <hongen.phe@antgroup.com>
2024-04-09 14:46:20 +0800

    Update the modules and adjust the format

--- flask4modelcache_demo.py
+++ flask4modelcache_demo.py
@@ -166,4 +166,5 @@ def user_backend():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
